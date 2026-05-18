ee4c9da845045ed7ecd737e32dc13c4ef0581997
Lucas Silva <silva.lucasdev@gmail.com>
2026-05-18 19:11:11 -0300

    chore: initial setup via Spring Initializr

---
+++ src/main/java/br/com/senai/projetointegrador/ProjetoIntegradorApplication.java
@@ -0,0 +1,13 @@
+package br.com.senai.projetointegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoIntegradorApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(ProjetoIntegradorApplication.class, args);
+	}
+
+}
